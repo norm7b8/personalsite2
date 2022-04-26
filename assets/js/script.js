0db24c89ee7041168606ecda23503fddccef12c2
Norman German <30629533+norm7b8@users.noreply.github.com>
2022-04-26 12:37:36 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// Final Project for WEB115-FON02 at GTCC
+// Coded by Norman German
+
+document.querySelector('.header__moon').onclick = function() {
+    document.querySelector('body').classList.toggle('is-black');
+}
+
+document.querySelector('.header__mob-menu').onclick = function() {
+    document.querySelector('.header-menu').classList.toggle('is-active');
+}
+
+
+
+
+
+
+
+
